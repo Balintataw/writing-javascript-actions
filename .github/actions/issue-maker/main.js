b1aa4b4615e973ff868282bd80a75657a2a4940c
Balintataw J <balintataw2@gmail.com>
2020-02-14 21:43:50 -0800

    Fix typo

--- .github/actions/issue-maker/main.js
+++ .github/actions/issue-maker/main.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+// const { Octokit } = require("@octokit/rest")
 
 async function run() {
   try {
@@ -8,15 +9,15 @@ async function run() {
     const token = core.getInput("repo-token");
 
     console.log("TOKEN", token);
-    const octokit = new github.GitHub(token);
-    console.log("OCTOKIT", octokit);
+    // const octokit = new github.GitHub(token);
+    // console.log("OCTOKIT", octokit);
 
-    const newIssue = await octokit.issues.create({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      title: issueTitle,
-      body: jokeBody
-    });
+    // const newIssue = await octokit.issues.create({
+    //   repo: github.context.repo.repo,
+    //   owner: github.context.repo.owner,
+    //   title: issueTitle,
+    //   body: jokeBody
+    // });
   } catch (error) {
     core.setFailed(error.message);
   }
