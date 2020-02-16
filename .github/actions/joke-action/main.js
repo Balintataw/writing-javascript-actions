e33c5df64a89bbb228ef8edccd4e5b1082aaf215
Balintataw J <balintataw2@gmail.com>
2020-02-16 14:37:44 -0800

    Test reviewers

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,10 +1,28 @@
 const getJoke = require("./joke");
 const core = require("@actions/core");
 
-async function run() {
-  const joke = await getJoke();
-  console.log("LOG", joke);
-  core.setOutput("joke-output", joke);
-}
+// async function run() {
+//   const joke = await getJoke();
+//   console.log("LOG", joke);
+//   core.setOutput("joke-output", joke);
+// }
 
+// run();
+
+async function run() {
+  try {
+    // const issueTitle = core.getInput("issue-title");
+    // const jokeBody = core.getInput("joke");
+    const token = core.getInput("repo-token");
+    const octokit = new github.GitHub(token);
+    const reviewers = await octokit.pulls.list({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner
+    });
+
+    console.log(reviewers);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
 run();
